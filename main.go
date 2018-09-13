3e10afd6d33e6338e66ad6339857b29cc4f3418f
Sean Abbott <sean.abbott@gmail.com>
2018-09-13 13:03:24 -0400

    finally got he profile finder working

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/hashicorp/hcl"
+	//	"github.com/hashicorp/hcl"
+	"github.com/spf13/viper"
 
 	flag "github.com/ogier/pflag"
 )
@@ -15,6 +16,7 @@ import (
 // flags
 var (
 	resource string
+	profile  string
 )
 
 // These structures were lifted from https://github.com/gruntwork-io/terragrunt/blob/master/remote/terraform_state_file.go
@@ -41,6 +43,10 @@ type TerraformStateModule struct {
 
 // end lifted structs
 
+type TerraformConfig struct {
+	Providers []map[string]interface{}
+}
+
 // helper functions
 func stringInSlice(a string, list []string) bool {
 	for _, b := range list {
@@ -51,9 +57,44 @@ func stringInSlice(a string, list []string) bool {
 	return false
 }
 
-//  https://stackoverflow.com/questions/37194739/how-check-a-file-contain-string-or-not-in-golang
-// It might be hacky but easier to just treat the ACL as string to get the profile
-func getAwsProfileFromFile() string {
+func getMapKeys(m map[string]interface{}) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// access a terraform file and determine if it has any aws profiles in it
+func getProfilesFromFile(f string) []string {
+	v := viper.New()
+	v.SetConfigType("hcl")
+	v.SetConfigFile(f)
+	if err := v.ReadInConfig(); err != nil {
+		fmt.Printf("Failed to read config file %s\n", f)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	var r []string
+	var p TerraformConfig
+	if v.InConfig("provider") {
+		p.Providers = v.Get("provider").([]map[string]interface{})
+	} else {
+		return r
+	}
+
+	for _, provider := range p.Providers {
+		keys := getMapKeys(provider)
+		if keys[0] == "aws" {
+			var aws_provider_map map[string]interface{}
+			aws_provider_map = provider["aws"].([]map[string]interface{})[0]
+			profile := aws_provider_map["profile"].(string)
+			r = append(r, profile)
+		}
+	}
+
+	return r
 }
 
 func getTerraformState() *TerraformState {
@@ -104,18 +145,36 @@ func getInstanceSlice(terraformState *TerraformState) []string {
 func main() {
 	flag.Parse()
 
+	fmt.Printf("Getting terraform state...")
 	terraformState := getTerraformState()
 	instanceSlice := getInstanceSlice(terraformState)
-	if flag.NFlag() == 0 {
+	if resource == "" {
 		fmt.Printf("Instances available: %v\n", instanceSlice)
 		os.Exit(0)
-	} else if stringInSlice(resource, instanceSlice) {
-		fmt.Printf("Unprotecting %s!\n", resource)
-	} else {
+	}
+
+	if !stringInSlice(resource, instanceSlice) {
 		fmt.Printf("Could not find resource %s. Instances available: %v\n", resource, instanceSlice)
 	}
+
+	if profile == "" {
+		p := getProfilesFromFile("core.tf")
+
+		if len(p) > 1 {
+			fmt.Printf("More than one profile found. Please select one from %v\n", p)
+			os.Exit(0)
+		} else if len(p) == 0 {
+			fmt.Printf("No aws profiles found. Attempting to use default.")
+			profile = "default"
+		} else {
+			profile = p[0]
+		}
+	}
+
+	fmt.Printf("Unprotecting %s using profile %s.\n", resource, profile)
 }
 
 func init() {
 	flag.StringVarP(&resource, "resource", "r", "", "Resource to unprotect")
+	flag.StringVarP(&profile, "profile", "p", "", "AWS profile to use")
 }
